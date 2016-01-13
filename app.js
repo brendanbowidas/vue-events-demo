1ba4065935e7dfee2f23e220aa23b4873f0c31e3
Brendan Bowidas <brendanbowidas@gmail.com>
2016-01-13 12:00:13 -0500

    add some more features

--- app.js
+++ app.js
@@ -4,16 +4,22 @@ Vue.component('location', {
   template: '#location',
   data: function(){
     return {
-      selected: '',
+      selected: null,
       locations: []
     };
   },
 created: function() {
+    console.log('created');
   this.fetchLocations();
+  if(this.$root.reservation.date){
+      console.log('date');
+    this.$dispatch('set-date', null);
+  }
+
 },
   methods: {
-    sendMsg: function() {
-      this.$dispatch('child-msg', this.selected);
+    setLocation: function() {
+      this.$dispatch('set-location', this.selected);
     },
     fetchLocations() {
             this.$http.get('http://homestead.app/api/product').then(function(response){
@@ -26,25 +32,30 @@ created: function() {
 
 
 
+
+
+
 // DATE COMPONENT
 
 Vue.component('date', {
     template: '#date',
     data: function() {
         return {
-            selected: '',
+            selected: null,
             dates: []
         }
     },
+
     created: function() {
       this.fetchDates();
+
     },
+
     methods: {
         fetchDates: function() {
-            this.$http.get('http://homestead.app/api/product/' + app.reservation.location_id + '?include=retailers').then(function(response){
+            this.$http.get('http://homestead.app/api/product/' + this.$root.reservation.location_id + '?include=retailers').then(function(response){
 
                this.dates = response.data[0].retailers;
-                console.log(this.dates);
             });
         },
         setDate: function() {
@@ -54,6 +65,45 @@ Vue.component('date', {
 });
 
 
+
+
+
+
+
+
+// TIMESLOTS COMPONENT
+Vue.component('timeslots', {
+    template: '#timeslots',
+    data: function(){
+        return {
+            selected: null,
+            timeslots: []
+        }
+    },
+    created: function() {
+     this.fetchTimeslots();
+    },
+    methods: {
+        fetchTimeslots: function() {
+            this.$http.get('http://homestead.app/api/product/' + this.$root.reservation.location_id + '?include=cleaning_tips').then(function(response){
+               this.timeslots = response.data[0].cleaning_tips;
+            });
+        },
+        setTime: function() {
+            this.$dispatch('set-timeslot', this.selected);
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+// APP COMPONENT
 var app = new Vue({
     el: 'body',
     data: function() {
@@ -63,13 +113,37 @@ var app = new Vue({
         };
     },
     events: {
-        'child-msg': function(msg) {
-            app.$set('reservation.location_id', msg);
-            this.step++;
+        'set-location': function(location) {
+            this.validate(location, function(location) {
+                app.$set('reservation.location_id', location);
+            });
+
         },
         'set-date': function(date) {
-            app.$set('reservation.date', date);
+            this.validate(date, function(date) {
+                app.$set('reservation.date', date);
+            });
+
+
+        },
+        'set-timeslot': function(time) {
+            this.validate(time, function(time) {
+                app.$set('reservation.timeslot', time);
+            });
+        }
+    },
+    methods: {
+        validate: function(item, callback) {
+            if(item === null) return false;
+            callback(item);
             this.step++;
+        },
+
+        back: function() {
+            if(this.step > 1){
+                this.step--;
+            }
+
         }
     }
 });
